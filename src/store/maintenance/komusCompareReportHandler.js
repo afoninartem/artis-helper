c0434ba127ce5ac055ad2dc2db18ef7e96c01bcb
afoninartem <afonin.art1988@gmail.com>
2022-07-02 17:56:01 +0300

    komus compare by sum

--- src/store/maintenance/komusCompareReportHandler.js
+++ src/store/maintenance/komusCompareReportHandler.js
@@ -1,71 +1,76 @@
 export default {
-	state: { komusReportData: null, komusCompareActData: null },
-	mutations: {
-		setKomusReportData(state, payload) {
-			state.komusReportData = payload;
-		},
-		setKomusCompareActData(state, payload) {
-			state.komusCompareActData = payload;
-		},
-	},
-	actions: {
-		async setKomusCompareActData(context, payload) {
-      const compareData = Array.from(payload).filter(d => d["Наименование документа"] === "Универсальный передаточный документ")
+  state: { komusReportData: null, komusCompareActData: null },
+  mutations: {
+    setKomusReportData(state, payload) {
+      state.komusReportData = payload;
+    },
+    setKomusCompareActData(state, payload) {
+      state.komusCompareActData = payload;
+    },
+  },
+  actions: {
+    async setKomusCompareActData(context, payload) {
+      const compareData = Array.from(payload).filter(
+        (d) =>
+          d["Наименование документа"] === "Универсальный передаточный документ"
+      );
       const unitedData = {};
-      compareData.forEach(item => {
-        unitedData[item["№ заказа"]] ? unitedData[item["№ заказа"]] += item["Дебет"] : unitedData[item["№ заказа"]] = item["Дебет"]
-      })
+      compareData.forEach((item) => {
+        unitedData[item["№ заказа"]]
+          ? (unitedData[item["№ заказа"]] += item["Дебет"])
+          : (unitedData[item["№ заказа"]] = item["Дебет"]);
+      });
       const result = {};
       for (let i in unitedData) {
         const sum = unitedData[i];
         const order = i;
-        result[sum] ? result[sum].push(order) : result[sum] = [order]
+        result[sum] ? result[sum].push(order) : (result[sum] = [order]);
       }
-    
-      return await context.commit("setKomusCompareActData", result)
-		},
-		async setKomusReportData(context, payload) {
-			const result = {
-				deliveredSum: 0,
-				canseledSum: 0,
-				delivered: {},
-				canseled: {},
-        deliveredBySum: {}
-			};
-			Array.from(payload).forEach((item) => {
-				const order = item["Номер заказа"];
-				const sum = item["Сумма в руб."].split(",").join(".");
-				if (item["Статус заказа"] !== "Отменен") {
-					if (!result.delivered[order]) result.delivered[order] = 0;
-					result.delivered[order] = (+result.delivered[order] + +sum).toFixed(
-						2
-					);
-					result.deliveredSum = (+result.deliveredSum + +sum).toFixed(2);
-				}
-				if (item["Статус заказа"] === "Отменен") {
-					if (!result.canseled[order]) result.canseled[order] = 0;
-					result.canseled[order] = (+result.canseled[order] + +sum).toFixed(2);
-					result.canseledSum = (+result.canseledSum + +sum).toFixed(2);
-				}
-			});
+
+      return await context.commit("setKomusCompareActData", result);
+    },
+    async setKomusReportData(context, payload) {
+      const result = {
+        deliveredSum: 0,
+        canseledSum: 0,
+        delivered: {},
+        canseled: {},
+        deliveredBySum: {},
+      };
+      Array.from(payload).forEach((item) => {
+        const order = item["Номер заказа"];
+        const sum = item["Сумма в руб."].split(",").join(".");
+        if (item["Статус заказа"] !== "Отменен") {
+          if (!result.delivered[order]) result.delivered[order] = 0;
+          result.delivered[order] = (+result.delivered[order] + +sum).toFixed(
+            2
+          );
+          result.deliveredSum = (+result.deliveredSum + +sum).toFixed(2);
+        }
+        if (item["Статус заказа"] === "Отменен") {
+          if (!result.canseled[order]) result.canseled[order] = 0;
+          result.canseled[order] = (+result.canseled[order] + +sum).toFixed(2);
+          result.canseledSum = (+result.canseledSum + +sum).toFixed(2);
+        }
+      });
       const del = result.deliveredBySum;
       for (let i in result.delivered) {
         // console.log(i)
         // console.log(result.delivered[i])
         const order = i;
-        const sum = +result.delivered[i]
-        del[sum] ? del[sum].push(order) : del[sum] = [order]
+        const sum = +result.delivered[i];
+        del[sum] ? del[sum].push(order) : (del[sum] = [order]);
         // result.deliveredBySum[result.delivered[i]] ? result.deliveredBySum[result.delivered[i]].push(result.delivered[i]) : result.deliveredBySum[result.delivered[i]] = []
-      } 
-			return await context.commit("setKomusReportData", result);
-		},
-	},
-	getters: {
-		getKomusReportData: (state) => {
-			return state.komusReportData;
-		},
-		getKomusCompareActData: (state) => {
-			return state.komusCompareActData;
-		},
-	},
+      }
+      return await context.commit("setKomusReportData", result);
+    },
+  },
+  getters: {
+    getKomusReportData: (state) => {
+      return state.komusReportData;
+    },
+    getKomusCompareActData: (state) => {
+      return state.komusCompareActData;
+    },
+  },
 };
--- src/views/maintenance/KomusReport.vue
+++ src/views/maintenance/KomusReport.vue
@@ -13,30 +13,21 @@
           {{ Number(komusReport.canseledSum).toLocaleString("ru-Ru") }} руб.
         </h1>
       </div>
+      <div v-if="komusCompareAct">{{ result }}</div>
       <table v-if="result">
-        <caption style="padding: 10px">
-          Проверить в акте Комуса
-        </caption>
         <thead>
           <tr>
-            <th>№ п/п</th>
-            <th>Номер счет-фактуры</th>
             <th>№ заказа</th>
             <th>Сумма</th>
-            <th>Комментарий</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(item, i) in result" :key="`result-${i}`">
-            <td>{{ item["№ п/п"] }}</td>
-            <td>{{ item["Номер счет-фактуры"] }}</td>
-            <td>{{ item["№ заказа"] }}</td>
-            <td>{{ item["Дебет"].toLocaleString("ru-Ru") }}</td>
-            <td>{{ item.description }}</td>
+          <tr v-for="order, o in result" :key="`order-${o}`">
+            <td>{{order[0]}}</td>
+            <td>{{Number(order[1]).toLocaleString("ru-Ru")}} руб.</td>
           </tr>
         </tbody>
       </table>
-      <!-- <div class="loader" v-if="!komus">Загрузите файл ↑</div> -->
     </div>
   </div>
 </template>
@@ -64,26 +55,18 @@ export default {
     },
     result() {
       if (!this.komusReport || !this.komusCompareAct) return;
-      const dataToCheck = [];
-      Array.from(this.komusCompareAct).forEach((item) => {
-        const sumActEntries = Array.from(this.komusCompareAct).filter(
-          (f) => f["Дебет"] === item["Дебет"]
-        ).length;
-        const sumReportEntries = Object.values(
-          this.komusReport.delivered
-        ).filter((f) => +f === item["Дебет"]).length;
-        sumActEntries !== sumReportEntries
-          ? ((item.description = `В акте ${sumActEntries} ${this.casesHandler(
-              sumActEntries,
-              "раз"
-            )}, в отчете ${sumReportEntries} ${this.casesHandler(
-              sumReportEntries,
-              "раз"
-            )}`),
-            dataToCheck.push(item))
-          : null;
-      });
-      return dataToCheck;
+      const dataToCheck = Object.entries(this.komusCompareAct).filter(
+        (el) =>
+          !Object.prototype.hasOwnProperty.call(
+            this.komusReport.deliveredBySum,
+            el[0]
+          )
+      );
+      return dataToCheck.map((arr) =>
+        Array.from(arr[1]).map((el) => {
+          return { [el]: arr[0] };
+        })
+      ).flat().map(el => Object.entries(el)).flat();
     },
   },
 };
