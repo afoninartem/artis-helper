ea3cab9f32b8efdfc940d51b94c24795ee278874
afoninartem <afonin.art1988@gmail.com>
2022-07-06 20:38:38 +0300

    Today workers for service

---
+++ src/views/service/TodayWorkingService.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="today-working">
+    <h1>Сегодня работают</h1>
+    <div class="today-working__grid">
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
