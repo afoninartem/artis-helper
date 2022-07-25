909311ca973dd7d3bfc1d1ca2deeac419bb5cbb2
afoninartem <afonin.art1988@gmail.com>
2022-07-25 11:34:24 +0300

    driver extras clear logic

--- src/store/service/driverExtraPopupHandler.js
+++ src/store/service/driverExtraPopupHandler.js
@@ -1,60 +1,61 @@
 import { db } from "../../main";
 
 export default {
-  state: {
-    show: false,
-    driver: null,
-    days: null,
-  },
-  mutations: {
-    openDriverExtraPopup(state, payload) {
-      state.show = true;
-      state.driver = payload.driver;
-      state.days = payload.days;
-    },
-    closeDriverExtraPopup(state) {
-      state.show = false;
-      state.driver = null;
-      state.day = null;
-    },
-  },
-  actions: {
-    async openDriverExtraPopup(context, payload) {
-      return await context.commit("openDriverExtraPopup", payload);
-    },
-    async closeDriverExtraPopup(context) {
-      return await context.commit("closeDriverExtraPopup");
-    },
-    async updateExtras({ getters }, payload) {
-      // console.log(payload.driverID, payload.days)
-      const driver = getters.getActualStates.catalogDrivers.filter(
-        (d) => d.driverID === payload.driverID
-      )[0];
-      driver.extras
-        ? // ? driver.extras.push(...payload.days)
-          Array.from(payload.days).forEach((day) => {
-            driver.extras.map((e) => e.day).includes(day.day)
-              ? driver.extras.splice(
-                  driver.extras.map((e) => e.day).indexOf(day.day),
-                  1,
-                  day
-                )
-              : driver.extras.push(day);
-          })
-        : (driver.extras = payload.days);
-      // console.log(driver);
-      await db
-        .collection("service/catalog/drivers")
-        .doc(payload.driverID)
-        .update({ extras: driver.extras });
-    },
-  },
-  getters: {
-    getDriverExtraPopupVisibility: (state) => {
-      return state.show;
-    },
-    getDriverExtraPopupDetails: (state) => {
-      return { driver: state.driver, days: state.days };
-    },
-  },
+	state: {
+		show: false,
+		driver: null,
+		days: null,
+	},
+	mutations: {
+		openDriverExtraPopup(state, payload) {
+			state.show = true;
+			state.driver = payload.driver;
+			state.days = payload.days;
+		},
+		closeDriverExtraPopup(state) {
+			state.show = false;
+			state.driver = null;
+			state.day = null;
+		},
+	},
+	actions: {
+		async openDriverExtraPopup(context, payload) {
+			return await context.commit("openDriverExtraPopup", payload);
+		},
+		async closeDriverExtraPopup(context) {
+			return await context.commit("closeDriverExtraPopup");
+		},
+		async updateExtras({ getters }, payload) {
+			// console.log(payload.driverID, payload.days)
+			const driver = getters.getActualStates.catalogDrivers.filter(
+				(d) => d.driverID === payload.driverID
+			)[0];
+			driver.extras
+				? Array.from(payload.days).forEach((day) => {
+						driver.extras.map((e) => e.day).includes(day.day)
+							? driver.extras.splice(
+									driver.extras.map((e) => e.day).indexOf(day.day),
+									1,
+									day
+							  )
+							: driver.extras.push(day);
+				  })
+				: (driver.extras = payload.days);
+			const extras = driver.extras.filter(e => e.cut.length > 0)
+      console.log(driver.extras)
+      console.log(extras)
+			await db
+				.collection("service/catalog/drivers")
+				.doc(payload.driverID)
+				.update({ extras: extras });
+		},
+	},
+	getters: {
+		getDriverExtraPopupVisibility: (state) => {
+			return state.show;
+		},
+		getDriverExtraPopupDetails: (state) => {
+			return { driver: state.driver, days: state.days };
+		},
+	},
 };
